a2652c62f7a86a67ecbdbf001f6dc42312a01b8d
mohayonao <mohayonao@gmail.com>
2013-02-25 09:22:25 +0900

    update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,67 +1,67 @@
 module.exports = function(grunt) {
-    "use strict";
-    
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-concat");
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-clean");
-    
-    grunt.initConfig({
-        watch: {
-            scripts: {
-                files: [
-                    "include/*.js", "include/ogg/*.js",
-                    "src/*.js", "test/*.js"
-                ],
-                tasks: ["concat", "jshint", "uglify"],
-                options: {
-                    interrupt: true
-                }
-            }
+  "use strict";
+  
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  
+  grunt.initConfig({
+    watch: {
+      scripts: {
+        files: ["include/*.js", "include/ogg/*.js", "src/*.js"],
+               
+        tasks: ["concat", "jshint:dest", "uglify"]
+      },
+      tests: {
+        files: ["test/*.js"],
+        tasks: ["concat", "jshint:test", "uglify"]
+      }
+    },
+    concat: {
+      dist: {
+        src: [
+          "build/header.txt",
+          "include/stdlib.js",
+          "include/ogg/ogg.h.js",
+          "src/*.js",
+          "include/exports.js",
+          "build/footer.txt"
+        ],
+        dest: "libogg.dev.js"
+      }
+    },
+    jshint: {
+      dest: ["libogg.dev.js"],
+      test: ["test/*.js"],
+      options: {
+        curly   : false,
+        eqeqeq  : true,
+        latedef : true,
+        noarg   : true,
+        noempty : true,
+        quotmark: "double",
+        undef   : true,
+        strict  : true,
+        trailing: true,
+        newcap  : true,
+        browser : true,
+        node    : true
+      }
+    },
+    uglify: {
+      all: {
+        options: {
+          sourceMap: "libogg.js.map"
         },
-        concat: {
-            dist: {
-                src: [
-                    "build/header.txt",
-                    "include/stdlib.js",
-                    "include/ogg/ogg.h.js",
-                    "src/*.js",
-                    "include/exports.js",
-                    "build/footer.txt"
-                ],
-                dest: "libogg.dev.js"
-            }
-        },
-        jshint: {
-            all: ["libogg.dev.js"],
-            options: {
-                curly   : false,
-                eqeqeq  : true,
-                latedef : true,
-                noarg   : true,
-                noempty : true,
-                quotmark: "double",
-                undef   : true,
-                strict  : true,
-                trailing: true,
-                newcap  : true,
-                browser : true,
-                node    : true
-            }
-        },
-        uglify: {
-            all: {
-                options: {
-                    sourceMap: "libogg.js.map"
-                },
-                files: {
-                    "libogg.js": ["libogg.dev.js"]
-                }
-            }
-        },
-        clean: ["libogg.dev.js", "libogg.js", "libogg.js.map"]
-    });
-    
-    grunt.registerTask("default", ["concat", "jshint", "uglify"]);
+        files: {
+          "libogg.js": ["libogg.dev.js"]
+        }
+      }
+    },
+    clean: ["libogg.dev.js", "libogg.js", "libogg.js.map"]
+  });
+  
+  grunt.registerTask("default", ["concat", "jshint", "uglify"]);
 };
